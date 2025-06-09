a3644925855e080890d47c0e865e239345551dc0
Your Name <your-email@example.com>
2025-06-10 00:20:10 +0200

    fix: Update paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })
 
